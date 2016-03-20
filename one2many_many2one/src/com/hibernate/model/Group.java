6eb407ee2946ff23af5c8d30721cb47a167de9af
tage <763409282@qq.com>
2016-03-20 16:09:57 +0800

    one to many, many to one

---
+++ one2many_many2one/src/com/hibernate/model/Group.java
@@ -0,0 +1,44 @@
+package com.hibernate.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by tage on 3/20/16.
+ */
+@Entity
+@Table(name = "t_group")
+public class Group {
+    private int id;
+    private String name;
+    private Set<User> users = new HashSet<>();
+
+    @OneToMany(mappedBy = "group")
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Id
+    @Column(name = "group_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ one2many_many2one/src/com/hibernate/model/User.java
@@ -0,0 +1,42 @@
+package com.hibernate.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by tage on 3/20/16.
+ */
+@Entity
+@Table(name = "t_user")
+public class User {
+    private int id;
+    private String name;
+    private Group group;
+
+    @ManyToOne
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
